c451eef39476cb3012f55239aaa76f85fcd18eb6
AngelsAltan <angelsebadtianam@gmail.com>
2024-04-08 21:44:02 -0600

    Read file and add to list

--- automatonApp/automatonApp/dfa.cs
+++ automatonApp/automatonApp/dfa.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
 
 namespace automatonApp
 {
+	
 	public partial class dfa : Form
 	{
+		// Lista global para almacenar la informacion del archivo
+		public static List<string> datafile = new List<string>();
 		public dfa()
 		{
 			InitializeComponent();
@@ -23,6 +28,26 @@ namespace automatonApp
 			{
 				//aqui va el codigo para que el usuario selecione el archivo y obtenga la ruta
 				String file_path = openFileDialog1.FileName;
+				try
+				{
+					// Utilizar streamreader para leer el archivo.
+					using (StreamReader file = new StreamReader(file_path))
+					{
+						string line;
+						// Leemos el archivo por linea.
+						while ((line = file.ReadLine()) != null)
+						{
+							datafile.Add(line);
+						}
+					}
+				}
+				catch (Exception a)
+				{
+					Console.WriteLine("The file could not be read:");
+					Console.WriteLine(a.Message);
+				}
+
+
 			}
 		}
 	}
