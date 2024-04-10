481a1e235689c68725200c8d65bef1d95c9f0114
AngelsAltan <angelsebadtianam@gmail.com>
2024-04-10 16:21:58 -0600

    Metodos de validacion de estados y transiciones

--- automatonApp/automatonApp/dfa.cs
+++ automatonApp/automatonApp/dfa.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.AxHost;
 
 namespace automatonApp
 {
@@ -20,7 +21,7 @@ namespace automatonApp
 		// Lista global para almacenar la informacion del archivo
 		public List<string> transitions = new List<string>();
 		public int num_states;
-		public string intial_state;
+		public string initial_state;
 		public List<string> final_states = new List<string>();
 		public int count = 0;
 		public bool error = false;
@@ -42,14 +43,42 @@ namespace automatonApp
 			bool isNumber = Regex.IsMatch(line, "^[1-9]+$");
 			return isNumber;
 		}
-		//utiliza el metodo number_comprobation y agrega la variante del poder ser varios
+		//Agrega la variante del poder ser varios
 		public void number_comprobation_final_states(string line)
 		{
 			final_states = line.Split(',').Select(num => num.Trim()).ToList();
 		}
-		//utiliza el metodo number_comprobation y agrega la variante de tener un simbolo
-		public void transitions_comprobation()
+		//Comprobacion del formato de las transiciones
+		public bool transitions_validations(List<string> transitions)
 		{
+			string actual_state, transition, state_to_reach;
+
+			for (int i = 0; i < transitions.Count; i++)
+			{
+				actual_state = transitions[i].Split(',')[0].Trim();
+				transition = transitions[i].Split(',')[1].Trim();
+				state_to_reach = transitions[i].Split(',')[2].Trim();
+				if (actual_state.Equals("") || transition.Equals("") || state_to_reach.Equals(""))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		//Valida y compara que los estados se encuentren en las transiciones.
+		public bool states_comprobation(List<string> transitions, string state)
+		{
+			string state_in_transition, state_in_transition2;
+			for (int i = 0; i < transitions.Count; i++ )			
+			{
+				state_in_transition = transitions[i].Split(',')[0].Trim();
+				state_in_transition2 = transitions[i].Split(',')[2].Trim();
+				if (state.Equals(state_in_transition) || state.Equals(state_in_transition2))
+				{
+					return true;
+				}
+			}
+			return false;
 
 		}
 
@@ -85,8 +114,10 @@ namespace automatonApp
 									}
 									break;
 								case 1://estado incial
-
+									initial_state = line;
+									count++;
 									break;
+
 								case 2://estados finales
 									try
 									{
@@ -115,6 +146,7 @@ namespace automatonApp
 					Console.WriteLine(a.Message);
 				}
 			}
+			states_comprobation(transitions, initial_state);
 		}
 
 		private void dfa_Load(object sender, EventArgs e)
